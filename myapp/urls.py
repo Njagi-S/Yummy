cd5727aa063256e255e719b607413a2094f15dc6
Njagi-S <andrewsheldonnjagi@gmail.com>
2024-11-17 16:17:57 +0300

    Second Commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,21 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('', views.index, name = 'index'),
+    
     path('starter/', views.starter, name = 'starter'),
+    
+    path('about/', views.about, name = 'about'),
+    
+    path('chefs/', views.chefs, name = 'chefs'),
+    
+    path('contact/', views.contact, name = 'contact'),
+    
+    path('events/', views.events, name = 'events'),
+    
+    path('menu/', views.menu, name = 'menu'),
+    
+    path('gallery/', views.gallery, name = 'gallery'),
+    
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -7,3 +7,21 @@ def index(request):
 
 def starter(request):
     return render(request, 'starter-page.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def chefs(request):
+    return render(request, 'chefs.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def events(request):
+    return render(request, 'events.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
