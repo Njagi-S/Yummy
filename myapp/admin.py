55e6e0ef27167e30bd6faf1edc19da9d48d2ee4b
Njagi-S <andrewsheldonnjagi@gmail.com>
2024-11-23 20:24:47 +0300

    Updated With Edit, Update and Delete Functionality and Dynamic Images.

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from myapp.models import BookTable, ContactTable
+from myapp.models import BookTable, ContactTable  # Importing the models to be registered in the admin panel
+
 # Register your models here.
+
+# Register the BookTable model in the Django admin interface
 admin.site.register(BookTable)
+
+# Register the ContactTable model in the Django admin interface
 admin.site.register(ContactTable)
