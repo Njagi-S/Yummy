f0b628552a49173d911ae3338258d2b01dd826c1
Njagi-S <andrewsheldonnjagi@gmail.com>
2024-11-24 22:15:19 +0300

    Updated with Xampp Support

--- Yummy/settings.py
+++ Yummy/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'Yummy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yummy',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'STRICT_TRANS_TABLES',
+        },
     }
 }
 
